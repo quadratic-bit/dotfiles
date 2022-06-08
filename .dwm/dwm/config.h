345fc049f05d154e88408c86b195e47bae1d6275
Artem <fynjyxbrjd@gmail.com>
2022-06-08 13:43:46 +0700

    dwm: add gaps

--- .dwm/dwm/config.h
+++ .dwm/dwm/config.h
@@ -5,8 +5,9 @@
 
 /* appearance */
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
+static const unsigned int gappx     = 6;
 static const unsigned int snap      = 32;       /* snap pixel */
-static const int showbar            = 1;        /* 0 means no bar */
+static const int showbar            = 0;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int user_bh            = 25;       /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
 static const char *fonts[]          = { "Droid Sans Mono:size=11", "FontAwesome:size=11", "SpaceMono Nerd Font Mono:size=11", "Ubuntu Mono Nerd Font:size=11" };
@@ -48,7 +49,7 @@ static const Rule rules[] = {
 /* layout(s) */
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
 static const Layout layouts[] = {
