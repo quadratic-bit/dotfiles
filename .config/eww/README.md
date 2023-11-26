07388535aae9553c2967fafff545b486bc59af32
April <fynjyxbrjd@gmail.com>
2023-11-26 15:41:36 +0700

    bar: eww -> waybar

--- .config/eww/README.md
+++
@@ -1,1 +0,0 @@
-Heavily inspired by [end-4](https://github.com/end-4/dots-hyprland)
--- .config/eww/scripts/change_volume.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-if [ "$1" = "down" ]; then
-    pamixer -d 1
-elif [ "$1" == "up" ]; then
-    pamixer -i 1
-else
-    echo "doomed"
-fi
